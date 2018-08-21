89602c0214ab24fcb1629a85e0d32445b7d7268b
Austin Powell <austinpowell58@gmail.com>
2018-08-21 23:14:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,9 @@
 class Transfer
-  def initialize(new_sender, new_eceiver, new_amount)
-    @sender = sender
+  
+  def initialize(new_sender, new_receiver, new_amount)
+    @sender = new_sender
+    @reciever = new_receiver
+    @amount = new_amount
+    @status = "pending"
+  end 
 end

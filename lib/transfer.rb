c5e4cea9407dd1098962041722f6e7b21dc7a274
Austin Powell <austinpowell58@gmail.com>
2018-08-21 23:15:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,8 @@
 class Transfer
   
+  attr_accessor :status
+  attr_reader :sender, :reciever, :amount 
+  
   def initialize(new_sender, new_receiver, new_amount)
     @sender = new_sender
     @reciever = new_receiver
